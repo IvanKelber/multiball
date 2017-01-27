cc4e09c76bf8dc6b32893c992d6b096dd2074039
Ivan Kelber <ivankelber@gmail.com>
2017-01-27 12:56:45 -0500

    new clients now see existing clients

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
+var Player = require('./static/assets/js/game.js').Player;
 //SET UP STATIC ASSETS FOLDER
 app.use(express.static(__dirname + '/static'));
 
@@ -14,19 +14,28 @@ http.listen(8000,function() {
   console.log("Listening on 8000");
 });
 
-var users = {};
+var players = {};
 
 //When a client connects
 io.on('connection',function(socket) {
   // console.log("a user connected");
   //
   // console.log(socket.conn.id);
-  //put user into table
-  users[socket.conn.id] = [0,0];
-  io.emit('update color',"red");
+  //put new player into table
+  var startX = Math.random()*45;
+  var startY = Math.random()*100;
+  var newPlayer =  new Player(startX,startY);
+  newPlayer.id = socket.conn.id;
+  players[socket.conn.id] = newPlayer;
+
+  io.emit('new client',{x:newPlayer.getX(),y:newPlayer.getY()}); //let other client know this player exists
+  for (var id in players) {
+    var player = players[id];
+    socket.emit('new client',{x:player.getX(),y:player.getY()});
+  }
   socket.on('disconnect',function() {
     //remove user from table
-    delete users[socket.conn.id];
+    delete players[socket.conn.id];
   });
 
 
